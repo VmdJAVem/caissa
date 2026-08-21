1be4280ee2c0ab74a40066f40398ed134f7ffcca
Luis Justo <unixlunatic@proton.me>
2026-08-20 22:20:02 -0500

    New files

---
+++ include/types.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <array>
+#include <cstdint>
+
+using Bitboard = std::uint64_t;
+
+enum class Color {
+	White,
+	Black
+};
+
+constexpr std::array<Color, 2> allColors = {
+	Color::White, Color::Black
+};
+
+enum class Piece {
+	Pawn,
+	Knight,
+	Bishop,
+	Rook,
+	Queen,
+	King,
+
+	None
+};
+constexpr std::array<Piece, 6> allPieces = {
+	Piece::Pawn, Piece::Knight, Piece::Bishop,
+	Piece::Rook, Piece::Queen, Piece::King
+};
+
+enum class Square : std::uint8_t {
+	A1, B1, C1, D1, E1, F1, G1, H1,
+	A2, B2, C2, D2, E2, F2, G2, H2,
+	A3, B3, C3, D3, E3, F3, G3, H3,
+	A4, B4, C4, D4, E4, F4, G4, H4,
+	A5, B5, C5, D5, E5, F5, G5, H5,
+	A6, B6, C6, D6, E6, F6, G6, H6,
+	A7, B7, C7, D7, E7, F7, G7, H7,
+	A8, B8, C8, D8, E8, F8, G8, H8,
+
+	None
+};
+
+struct PieceOnSquare {
+	Color color;
+	Piece piece;
+};
+
+enum class CastlingRights : std::uint8_t {
+	None         = 0,
+	WhiteKingside  = 1 << 0,  // 0b0001
+	WhiteQueenside = 1 << 1,  // 0b0010
+	BlackKingside  = 1 << 2,  // 0b0100n
+	BlackQueenside = 1 << 3,  // 0b1000
+	All = WhiteKingside | WhiteQueenside | BlackKingside | BlackQueenside
+};
+
+constexpr CastlingRights operator|(CastlingRights a, CastlingRights b) {
+	return static_cast<CastlingRights>(static_cast<uint8_t>(a) | static_cast<uint8_t>(b));
+}
+constexpr CastlingRights operator|=(CastlingRights& a, CastlingRights b) {
+	a = a | b;
+	return a;
+}
+constexpr CastlingRights operator&(CastlingRights a, CastlingRights b) {
+	return static_cast<CastlingRights>(static_cast<uint8_t>(a) & static_cast<uint8_t>(b));
+}
+constexpr CastlingRights operator~(CastlingRights a) {
+	return static_cast<CastlingRights>(~static_cast<uint8_t>(a) & static_cast<uint8_t>(CastlingRights::All));
+}
+constexpr CastlingRights& operator&=(CastlingRights& a, CastlingRights b) {
+	a = a & b;
+	return a;
+}
+
+struct UndoInfo {
+	CastlingRights previousCastlingRights;
+	Square previousEnPassantTarget;
+	int previousHalfMoveClock;
+};
---
+++ src/move_application.cpp
@@ -0,0 +1,100 @@
+#include "board.hpp"
+#include <cassert>
+
+UndoInfo Board::makeMove(Move move) {
+	UndoInfo undo = {.previousCastlingRights = m_castlingRights, .previousEnPassantTarget = m_enPassantTarget, .previousHalfMoveClock = m_halfMoveClock};
+	Color opposite = m_sideToMove == Color::White ? Color::Black : Color::White;
+	m_enPassantTarget = Square::None;
+	
+	if (move.capturedPiece != Piece::None && !move.isEnPassant) {
+		removePiece(opposite, move.capturedPiece, move.to);
+		m_halfMoveClock = 0;
+	} else if (move.isEnPassant) {
+		int capturedPawnIndex = (m_sideToMove == Color::White) ? static_cast<int>(move.to) - 8 : static_cast<int>(move.to) + 8;
+		removePiece(opposite, Piece::Pawn, static_cast<Square>(capturedPawnIndex));
+		m_halfMoveClock = 0;
+	} else if (move.piece == Piece::Pawn) {
+		m_halfMoveClock = 0;
+		int toIndex = static_cast<int>(move.to);
+		int fromIndex = static_cast<int>(move.from);
+		int diff = toIndex - fromIndex;
+		if (diff == 16 || diff == -16) {
+			int enPassantIndex = (m_sideToMove == Color::White) ? toIndex - 8 : toIndex + 8;
+			m_enPassantTarget = static_cast<Square>(enPassantIndex);
+		}
+	} else {
+		m_halfMoveClock++;
+	}
+
+	if (move.isCastling) {
+		switch (move.to) {
+		case Square::G1:
+			placePiece(m_sideToMove, Piece::Rook, Square::F1);
+			removePiece(m_sideToMove, Piece::Rook, Square::H1);
+			break;
+		case Square::C1:
+			placePiece(m_sideToMove, Piece::Rook, Square::D1);
+			removePiece(m_sideToMove, Piece::Rook, Square::A1);
+			break;
+		case Square::G8:
+			placePiece(m_sideToMove, Piece::Rook, Square::F8);
+			removePiece(m_sideToMove, Piece::Rook, Square::H8);
+			break;
+		case Square::C8:
+			placePiece(m_sideToMove, Piece::Rook, Square::D8);
+			removePiece(m_sideToMove, Piece::Rook, Square::A8);
+			break;
+		default:
+			assert(false);
+			break;
+		}
+	}
+
+	if (move.piece == Piece::King) {
+		if (m_sideToMove == Color::White)
+			m_castlingRights &= ~(CastlingRights::WhiteKingside | CastlingRights::WhiteQueenside);
+		else
+			m_castlingRights &= ~(CastlingRights::BlackKingside | CastlingRights::BlackQueenside);
+	}
+	else if (move.piece == Piece::Rook) {
+		switch (move.from) {
+		case Square::A1:
+			m_castlingRights &= ~CastlingRights::WhiteQueenside;
+			break;
+		case Square::H1:
+			m_castlingRights &= ~CastlingRights::WhiteKingside;
+			break;
+		case Square::A8:
+			m_castlingRights &= ~CastlingRights::BlackQueenside;
+			break;
+		case Square::H8:
+			m_castlingRights &= ~CastlingRights::BlackKingside;
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (move.capturedPiece == Piece::Rook) {
+		switch (move.to) {
+		case Square::A1: m_castlingRights &= ~CastlingRights::WhiteQueenside; break;
+		case Square::H1: m_castlingRights &= ~CastlingRights::WhiteKingside; break;
+		case Square::A8: m_castlingRights &= ~CastlingRights::BlackQueenside; break;
+		case Square::H8: m_castlingRights &= ~CastlingRights::BlackKingside; break;
+		default: break;
+		}
+	}
+	
+	if (move.promotionPiece != Piece::None) {
+		placePiece(m_sideToMove, move.promotionPiece, move.to);
+	}
+	else 
+		placePiece(m_sideToMove, move.piece, move.to);
+	removePiece(m_sideToMove, move.piece, move.from);
+
+	if (m_sideToMove == Color::Black)
+		m_fullMoveNumber++;
+
+	m_sideToMove = opposite;
+	return undo;
+}
